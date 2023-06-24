91aea3d45616c0d31d4371f150c9a343bb831943
Rahul kardile <rahulkardile321@gmail.com>
2023-06-24 23:01:37 +0530

    props and ites uses

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Contact from "../src/components/Contact"
+import Card from './components/Card';
+
+console.log(Contact);
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <h1 className="heading">My Contacts</h1>
+    <Card 
+      name={Contact[0].name}
+      img={Contact[0].imgURL}
+      tel={Contact[0].phone}
+      email={Contact[0].email}
+    />
+    <Card 
+      name={Contact[1].name}
+      img={Contact[1].imgURL}
+      tel={Contact[1].phone}
+      email={Contact[1].email}
+    />
+    <Card 
+      name={Contact[2].name}
+      img={Contact[2].imgURL}
+      tel={Contact[2].phone}
+      email={Contact[2].email}
+    />
+    
+  </div>
   );
 }
+
 
 export default App;
---
+++ src/components/Contact.js
@@ -0,0 +1,24 @@
+const Contacts = [
+    {
+    name: "Sushant Singh Rajput ",
+    imgURL:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQHJuWeNYZI9_DOdFLsw5DJ14-e_Kr613BCQ&usqp=CAU",
+    phone: "+123 456 789",
+    email: "Sush@beyonce.com"
+},
+{
+    name: "Jack Bauer",
+    imgURL:
+        "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+    phone: "+987 654 321",
+    email: "jack@nowhere.com"
+},
+{
+    name: "Chuck Norris",
+    imgURL:
+        "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+    phone: "+918 372 574",
+    email: "gmail@chucknorris.com"
+}]
+
+export default Contacts;
